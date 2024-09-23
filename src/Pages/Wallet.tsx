a2f6047a7256a435587c676ee14620246d4251e2
unknown <minhalalikhan.786@gmail.com>
2024-09-23 11:29:28 +0530

    images added, navbar updated

--- src/Pages/Wallet.tsx
+++ src/Pages/Wallet.tsx
@@ -4,9 +4,16 @@ type Props = {};
 
 function Wallet({}: Props) {
   return (
-    <div className="flex flex-col w-full h-full">
+    <div className="flex flex-col w-full h-full ">
       <Navbar />
-      Wallet
+      <div className="flex flex-col w-full flex-1 justify-center items-center">
+        <div className="flex flex-col h-full max-w-[1440px] w-full bg-slate-500 p-10">
+          {/* Header  */}
+          <div>Wallet</div>
+          {/* Transaction history */}
+          <div className=" w-full flex flex-col flex-[2]"></div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,21 @@
 import { Link } from "react-router-dom";
+import { FaAngleDown } from "react-icons/fa";
+import { FaClipboard } from "react-icons/fa";
+import Solana from "../assets/solana.svg";
 
 type Props = {};
 
 function Navbar({}: Props) {
   return (
-    <div className=" w-full h-16 border-b-2 flex flex-row justify-center">
-      <div className="w-full max-w-[1440px] flex flex-col  justify-center">
-        <h1 className="font-bold mx-auto">Cryptoid</h1>
-        <div className=" flex flex-row w-full relative justify-between">
+    <div className=" w-full  border-b-1 border-slate-600 flex flex-row justify-center">
+      <div className="w-full max-w-[1440px] flex flex-col  justify-center px-2 py-1">
+        <h1 className="font-bold mx-auto text-white p-2">Cryptoid</h1>
+        <div className=" flex flex-row w-full relative justify-center">
           <Link to={"/account"}>
             <AccountButton name="Minhal Ali Khan" />
           </Link>
           <Link to={"/walletmanager"}>
-            <WallettButton name="Wallet 1" logo="" />
+            <WallettButton name="Wallet 1" logo={Solana} />
           </Link>
         </div>
       </div>
@@ -29,7 +32,7 @@ function AccountButton({ name = "" }: AccountButtonProps) {
   Initials = Initials[0][0] + Initials[Initials.length - 1][0];
   console.log(Initials);
   return (
-    <div className="w-8 h-8 flex justify-center items-center font- text-sm rounded-full bg-red-900 text-red-500">
+    <div className="w-8 h-8 flex justify-center items-center font- text-sm rounded-full bg-red-950 text-red-500 absolute left-0 top-0">
       <p>{Initials}</p>
     </div>
   );
@@ -39,9 +42,18 @@ type WalletButtonProps = { name: string; logo: string };
 
 function WallettButton({ name = "", logo = "" }: WalletButtonProps) {
   return (
-    <div className="rounded-lg px-4 py-1 border-slate-100 bg-slate-800 text-white">
-      {logo && <img src={logo} />}
-      <p>{name}</p>
+    <div className="rounded-lg  border-slate-500 border bg-gray-800  text-white flex flex-row">
+      <div className="rounded-l-lg p-2  min-w-[50px] hover:bg-gray-900 flex justify-center items-center">
+        {logo && <img style={{ width: "20px" }} src={logo} />}
+      </div>
+      <div className="min-w-[100px] p-2 hover:bg-gray-900 border-x border-collapse  border-slate-500  flex justify-center items-center">
+        <p className="flex-1">{name}</p>
+        <FaAngleDown className="text-gray-500" />
+      </div>
+
+      <div className="rounded-r-lg min-w-[50px] p-2 hover:bg-gray-900  flex justify-center items-center">
+        <FaClipboard className="text-gray-500" />
+      </div>
     </div>
   );
 }
